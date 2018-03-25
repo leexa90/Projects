431a119e87d183180a2e159ef56a8f4aec0b6c6c
leexa90 <lee.x.a90@gmail.com>
2018-03-25 23:46:36 +0800

    all

--- toxicity/Final/Permability/all_seq/staple/temp.py
+++ toxicity/Final/Permability/all_seq/staple/temp.py
@@ -1,6 +1,7 @@
 from rdkit import Chem
 from rdkit.Chem import AllChem
 from rdkit.Chem import Descriptors
+import pandas as pd
 #[C@H]
 dictt = {'A':'N[C@H](C)C(=O)',
          'C':'N[C@H](CS)C(=O)',
@@ -9,17 +10,17 @@ dictt = {'A':'N[C@H](C)C(=O)',
          'F':'N[C@H](Cc1ccccc1)C(=O)',
          'G':'NCC(=O)',
          'H':'N[C@H](Cc1c[nH]cn1)C(=O)',
-         'I':'N[C@H](C(C)CC)C(=O)',#NC([C@@H](CC)C)C(=O)
+         'I':'N[C@H]([C@@H](C)CC)C(=O)',#NC([C@@H](CC)C)C(=O)
          'K':'N[C@H](CCCCN)C(=O)',
          'L':'N[C@H](CC(C)C)C(=O)',
          'M':'N[C@H](CCSC)C(=O)',
          'N':'N[C@H](CC(N)=O)C(=O)',
-         'NL':'N[C@@H](CC(N)=O)C(=O)'
+         'NL':'N[C@@H](CC(N)=O)C(=O)',
          'P':'N1CCCC1C(=O)',
          'Q':'N[C@H](CCC(N)=O)C(=O)',
          'R':'N[C@H](CCCNC(=N)N)C(=O)',
          'S':'N[C@H](CO)C(=O)',
-         'T':'N[C@H](C(O)C)C(=O)', #NC([C@H](O)C)C(=O)
+         'T':'N[C@H]([C@H](O)C)C(=O)', #NC([C@H](O)C)C(=O)
          'V':'N[C@H](C(C))C(=O)',
          'W':'N[C@H](Cc1c[nH]c2ccccc12)C(=O)',
          'Y':'N[C@H](Cc1ccc(O)cc1)C(=O)',
@@ -27,16 +28,72 @@ dictt = {'A':'N[C@H](C)C(=O)',
          'S8':'N[C@](CCCCCCC=2)(C)C(=O)',
          'R5':'N[C@@](CCCC=2)(C)C(=O)',
          'R8':'N[C@@](CCCCCCC=2)(C)C(=O)',
+         'S53':'N[C@](CCCC=3)(C)C(=O)',
+         'S83':'N[C@](CCCCCCC=3)(C)C(=O)',
+         'R53':'N[C@@](CCCC=3)(C)C(=O)',
+         'R83':'N[C@@](CCCCCCC=3)(C)C(=O)',
+         'B8':'NC(CCCC=2)(CCCC=3)C(=O)', #???
          'B5':'NC(CCCC=2)(CCCC=3)C(=O)',#'S8':'N[C@](CCCCCCC=3)(C)C(=O)' for S8
-         'BetaA':'NCCC(=O)',
+         'BAla':'NCCC(=O)',
          'PEG1':'NCCOCCOCC(=O)',
          'PEG2':'NCCOCOCCOCCC(=O)',
          'PEG5':'NCCOCCOCCOCCOCCOCCOCCC(=O)',
          'EEA':'NCCOCCOCCNC(=O)CCC(=O)',
-         'PPF':'N[C@@H](Cc1(F)c(F)c(F)c(F)c(F)c1(F))C(=O)',
+         'pff':'N[C@@H](Cc1c(F)c(F)c(F)c(F)c1(F))C(=O)',
          'FITC':'C(NC1=CC=C2C(=C1)C4(OC2=O)C3=C(C=C(C=C3)O)OC5=C4C=CC(=C5)O)(=S)'}
          
+
+
+         
+data  = pd.read_csv('dataset_stapled.csv',header=None)
          
          
-         
-         
+def process_string(str):
+    result = []
+    special_resi = False
+    for i in str:
+        if i =='(':
+            return result
+        elif special_resi and i !='-': #inside special residue
+            temp += i
+        elif i =='-':
+            if special_resi : #closing of special residue
+                result += [temp,]
+                special_resi = False
+                temp  = ''
+            else: #starting of special residue
+                special_resi = True 
+                temp = ''
+        else: #normal residue
+            result += [i,]
+    return result
+
+def save_smile(i,counter ):
+    smile = process_string(i)
+    smile_str = ''
+    second_ii = False
+    for ii in smile:
+        if second_ii==0.5 and ii in ['B5','B8']:
+            second_ii = True
+        elif second_ii is True and ii in ['S8','S5','R8','R5']:
+            second_ii = False
+            ii = ii +'3'
+        smile_str += dictt[ii]
+        if second_ii is  False and ii in ['S8','S5','R8','R5']:
+            second_ii = 0.5
+    smile_str += 'O'
+    try:
+        mol0 = Chem.MolFromSmiles(smile_str)
+        mol = Chem.AddHs(mol0)
+        AllChem.Compute2DCoords(mol)
+        AllChem.EmbedMolecule(mol,AllChem.ETKDG())
+        open('temp_%s.sdf'%(counter),'w').write(Chem.MolToMolBlock(mol))
+    except : print (counter,smile_str)
+
+
+data['list'] = data[1].apply(process_string)
+print (data[1].apply(process_string))
+counter = 0
+for i in data[1]:
+    smile = save_smile(i,counter = counter)
+    counter += 1
---
+++ toxicity/Final/Permability/all_seq/staple/temp2.py
@@ -0,0 +1,4 @@
+from pubchempy import *
+
+c = Compound.from_cid(1423)
+cs = get_compounds('Aspirin', 'name')
