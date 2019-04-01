d72729b67219ef526c2714529ed9a22748b8f924
leexa90 <lee.x.a90@gmail.com>
2019-04-01 15:49:37 +0800

    gradeint boost;

--- gradient_boosting/code.py
+++ gradient_boosting/code.py
@@ -31,15 +31,22 @@ y = y_ori - ensemble
 print np.mean((y_ori-ensemble)**2)
 for i in range(100):
     regressor = DecisionTreeRegressor(random_state=0,
-                                      max_depth=2).fit(X,y)
+                                      max_depth=1).fit(X,y)
     #print B
     ym = regressor.predict(X)
     y =y-0.01*ym
     ensemble += ym
-    if i%10 ==0:
-        plt.plot(X[:,0],y_ori,'bo')
-        plt.plot(X[:,0],ensemble,'r')
-        plt.show()
+    if i%1 ==0:
+        plt.plot(X[:,0],y_ori,'bo',label='data')
+        plt.plot(X[:,0],ensemble,'r',label='boosted predictions')
+        plt.legend(loc='upper right')
+        num = '{0:03d}'.format(i)
+        plt.title('Boosted  descision trees num %s'%num)
+        plt.xlabel('X var')
+        plt.ylabel('prediction')
+        plt.savefig('{0:03d}'.format(i)+'png')
+        plt.close()
     y = y_ori - ensemble
     print np.mean((y_ori-ensemble)**2)
-    
+#ffmpeg -framerate 1  -pattern_type glob -i '*.png' -filter:v tblend video.mp4
+#convert -loop 0 *png out.gif
